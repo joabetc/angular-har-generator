d7427e3710cae094bc1d876687c6a1fef59c953a
Joabe Costa <joabetc@hotmail.com>
2022-07-02 20:51:59 +0200

    Create builder

---
+++ src/app/builder.ts
@@ -0,0 +1,17 @@
+type Builder<T> = {
+  [k in keyof T]: (arg: T[k]) => Builder<T>
+} & { build(): T }
+
+export function proxyBuilder<T>(): Builder<T> {
+  var built: any = {};
+  var builder = new Proxy({}, {
+    get: function(target, prop, receiver) {
+      if (prop === 'build') return () => built;
+      return (x: any): any => {
+        (built[prop] = x);
+        return builder;
+      }
+    }
+  });
+  return builder as any;
+}
