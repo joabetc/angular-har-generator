74ffb16ee0c5e8526dc7407151dd85b52f1e633a
Joabe Costa <joabetc@hotmail.com>
2022-07-02 20:52:49 +0200

    Use HarRequest

--- src/app/har-entry.ts
+++ src/app/har-entry.ts
@@ -1,3 +1,5 @@
+import { HarRequest } from "./har-request";
+
 export interface HarEntry {
   _initiator: {
     type: 'script' | 'parser' | 'other';
@@ -12,71 +14,7 @@ export interface HarEntry {
   /**
    * Represents a single http request inside the HTTP Archive.
    */
-  request: {
-    /**
-     * The request method.
-     */
-    method: string;
-    /**
-     * The absolute url of the request (excluding fragments).
-     */
-    url: string;
-    /**
-     * The version of the http request
-     */
-    httpVersion: string;
-    /**
-     * A list of http header objects.
-     */
-    headers: {
-      name: string;
-      value: string;
-    }[];
-    /**
-     * A list of the individual objects in the query string
-     */
-    queryString: {
-      name: string;
-      value: string;
-    }[];
-    /**
-     * A list of cookie objects
-     */
-    cookies: {
-      /**
-       * The name of the cookie.
-       */
-      name: string;
-      /**
-       * The value of the cookie.
-       */
-      value: string;
-      /**
-       * The expiry date (if any) of the cookie.
-       */
-      expires: Date;
-      /**
-       * If the cookie is marked as httpOnly
-       */
-      httpOnly: boolean;
-      /**
-       * If the cookie is marked as secure, to only be transmitted over https.
-       */
-      secure: boolean;
-    }[];
-    /**
-     * The total number of bytes in the http request up to and including the double CRLF before the start of the request body
-     */
-    headersSize: number;
-    /**
-     * The total number of bytes in the http request body
-     */
-    bodySize: number;
-    postData?: {
-      mimeType: string;
-      text: string;
-    };
-  };
+  request: HarRequest;
   response: {
     /**
      * The numeric status of the response.
